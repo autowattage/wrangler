003e6399964c6d141713bea0dff6b36cd42854fa
autowattage <pipandw@gmail.com>
2026-07-29 14:18:40 -0400

    docs wip

--- js/landing.js
+++ js/landing.js
@@ -3,7 +3,7 @@ var media_tablet = window.matchMedia("(min-width: 40rem)");
 
 function autoclose_details() {
   document.getElementById("node-viewer").open = true;
-  document.getElementById("node-img").open = false;
+  document.getElementById("node-img").open = media_tablet.matches;
   document.getElementById("node-hsl").open = media_tablet.matches;
 }
 media_tablet.addEventListener("change", function() { autoclose_details(); });
