e753e3dd3fae4555cacae6eec67a5cb05e40cc07
autowattage <pipandw@gmail.com>
2026-08-09 20:07:30 -0400

    YAY ITS DONE

--- js/joinbutton.js
+++ js/joinbutton.js
@@ -1,29 +1,43 @@
 const emailfield = document.getElementById("flow-mail");
 
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 document.getElementById("joinbutton").addEventListener("click", async () => {
-  try {
-    const response = await fetch("/api/add-to-airtable", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email: emailfield.value
-      })
-    });
+  if (validateEmail(emailfield.value)) {
+    try {
+      const response = await fetch("/api/add-to-airtable", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: emailfield.value
+        })
+      });
 
-    const text = await response.text();
+      const text = await response.text();
 
-    console.log("HTTP status:", response.status);
-    console.log("Server response:", text);
+      console.log("HTTP status:", response.status);
+      console.log("Server response:", text);
 
-    if (response.ok) {
-      console.log("Added to Airtable!");
-    } else {
-      console.error("Request failed:", text);
+      if (response.ok) {
+        console.log("Added to Airtable!");
+        document.getElementById("joinbutton").textContent = "Email sent!";
+      } else {
+        console.error("Request failed:", text);
+      }
+    } catch (error) {
+      console.error("Fetch failed:", error);
+      document.getElementById("joinbutton").textContent = "Error, try again?";
     }
-  } catch (error) {
-    console.error("Fetch failed:", error);
+  } else {
+    document.getElementById("joinbutton").textContent = "invalid email :(";
   }
 });
 
