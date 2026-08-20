3785445e944066a74e7cc7a8a9b8af09994e925f
autowattage <pipandw@gmail.com>
2026-08-20 16:53:47 -0400

    whys this in here

--- public/js/shop.js
+++ public/js/shop.js
@@ -37,46 +37,6 @@ popup.addEventListener("click", (event) => { if (!event.target.closest('details'
 // another exit button
 exit.addEventListener("click", (event) => { popup.style.display = "none"; });
 
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      error: "Method not allowed"
-    });
-  }
-
-  const { email } = req.body;
-
-  try {
-    const response = await fetch(
-      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}`,
-      {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${process.env.AIRTABLE_TOKEN}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          fields: {
-            Email: email
-          }
-        })
-      }
-    );
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      return res.status(response.status).json(data);
-    }
-
-    return res.status(200).json(data);
-
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message
-    });
-  }
-}
 // purchase button
 purchase.addEventListener("click", (event) => {
   console.log("bought");
