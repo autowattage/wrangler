97a8f490dc9cdee668ab612d669622ded9c18965
autowattage <pipandw@gmail.com>
2026-08-24 13:05:34 -0400

    please let this work

--- api/getstats.js
+++ api/getstats.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
           body: new URLSearchParams({
             "client_id": process.env.HACKCLUB_CLIENT_ID,
             "client_secret": process.env.HACKCLUB_CLIENT_SECRET,
-            "redirect_uri": process.env.HACKCLUB_REDIRECT_URI || "http://localhost:3000/shop/",
+            "redirect_uri": "https://wrangler-git-shop-test-branch.hackclub.dev/shop/",
             "code": code,
             "grant_type": "authorization_code"
           }),
